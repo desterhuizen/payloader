b254e9b0bdaaaeed6c2447d5d89c3b7c12057ce0
desterhuizen <desterhuizen@gmail.com>
2025-04-01 08:13:01 +0200

    fixing key variable name

--- partial/xor.c
+++ partial/xor.c
@@ -1,7 +1,7 @@
 size_t arraysize = (int) sizeof(buf) -1;
-size_t keysize = (int) sizeof(xor_key) -1;
+size_t keysize = (int) sizeof(key) -1;
 int i;
 for (int i=0; i<arraysize; i++)
 {
-	buf[i] ^= xor_key[i%keysize];
+	buf[i] ^= key[i%keysize];
 }
